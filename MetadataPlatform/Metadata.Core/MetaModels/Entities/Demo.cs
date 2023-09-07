f274475051cffcf04fdcfd8a8b358427cdefcf75
k000 <AtaLuZiK@gmail.com>
2023-09-07 16:27:39 +0800

    demo

---
+++ MetadataPlatform/Metadata.Core/MetaModels/Entities/Demo.cs
@@ -0,0 +1,46 @@
+﻿namespace MetaModels.Entities;
+
+public class UnitEntity
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class MaterialEntity
+{
+    public Guid Id { get; set; }
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public Guid UnitId { get; set; }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// 对象元信息
+
+public class UnitObjectMeta : ObjectMeta
+{
+    public override Type RuntimeType => typeof(MaterialEntity);
+    public PropertyMeta Name { get; } = new PropertyMeta<string>("Name");
+}
+
+public class MaterialObjectMeta : ObjectMeta
+{
+    public override Type RuntimeType => typeof(MaterialEntity);
+    public PropertyMeta Name { get; } = new PropertyMeta<string>("Name");
+    public PropertyMeta Code { get; } = new PropertyMeta<Guid>("Code");
+    public PropertyMeta Unit { get; } = new PropertyMeta<UnitBo>("Unit");
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+public class UnitBo
+{
+    public string Name { get; set; }
+}
+
+public class MaterialBo
+{
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public UnitBo Unit { get; set; }
+}
---
+++ MetadataPlatform/Metadata.Core/MetaModels/Entities/ObjectMeta.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+namespace MetaModels.Entities;
+
+public abstract class ObjectMeta
+{
+    public abstract Type RuntimeType { get; }
+}
+
+public abstract class PropertyMeta
+{
+    public abstract Type RuntimeType { get; }
+    public abstract string Name { get; }
+}
+
+public class PropertyMeta<T> : PropertyMeta
+{
+    public override Type RuntimeType => typeof(T);
+    public override string Name { get; }
+
+    public PropertyMeta(string name)
+    {
+        Name = name;
+    }
+}
